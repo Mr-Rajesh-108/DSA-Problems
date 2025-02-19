48aad9325edd8bc1f9d28daf64bd5017e53ac147
Rajesh Chaurasiya <120089887+Mr-Rajesh-108@users.noreply.github.com>
2025-02-19 23:19:16 +0530

    add comment for better understanding..

--- strings/removePunctuation .js
+++ strings/removePunctuation .js
@@ -1,7 +1,7 @@
 function removePunctuation(str) {
-    return str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?"']/g, "");
+    return str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?"']/g, "");  //remove all type of Puctuation
 }
 
 let text = "Hello, world! How's it going?";
 let cleanText = removePunctuation(text);
-console.log(cleanText); // Output: "Hello world Hows it going"
+console.log(cleanText);     // Output: "Hello world Hows it going"
