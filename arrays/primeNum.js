ca270c6cdc565e8318e5803bb1e2ff9958eebd37
Rajesh Chaurasiya <120089887+Mr-Rajesh-108@users.noreply.github.com>
2025-04-05 11:35:22 +0530

    Add comments for Understandig

--- arrays/primeNum.js
+++ arrays/primeNum.js
@@ -4,23 +4,17 @@ const primeNum = (num) => {
     for (let i = 2; i <= num; i++) {
         let isPrime = true;
 
-     // Check if 'i' is divisible by any number from 2 to √i
-        for (let j = 2; j * j <= i; j++) {
-            // If divisible, it's not a prime number
-            if (i % j === 0) {
+        for (let j = 2; j * j <= i; j++) {         // Check if 'i' is divisible by any number
+            if (i % j === 0) {          // If divisible, it's not a prime number
                 isPrime = false;
                 break; // Exit loop early since we found a divisor
             }
         }
-
         if (isPrime) {
             arr.push(i);
         }
     }
 
-    // Print all prime numbers up to 'num'
     console.log("Prime numbers:", arr);
 };
-
-// Call the function with the value 10
 primeNum(100);
